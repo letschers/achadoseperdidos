db6fe9a264293248b717abd3d004e73e6fa80f42
letschers <eduardoletsch@hotmail.com>
2018-09-13 12:30:34 -0300

    Validations etc

--- app/models/dado.rb
+++ app/models/dado.rb
@@ -6,6 +6,12 @@ class Dado < ApplicationRecord
 	validates :tipo, presence: true, acceptance: {accept: ['achado', 'perdido']} 
 	validate :valid_date?
 	validates :foto, presence: true, null: false
+	validates :descricao, length: {
+		minimum: 20,
+		maximum: 200,
+
+		too_short: "curta demais!",
+		too_long: "longa demais"}
 
 	def valid_date?
 		if !self.dataocorrido.present? or self.dataocorrido > Date.today
